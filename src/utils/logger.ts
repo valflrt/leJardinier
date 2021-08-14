f97f57781fc70d906b653cedb35b6a244aaccd97
valflrt <vfleurit@gmail.com>
2021-08-14 11:22:53 +0200

    added functions write and success

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -3,22 +3,32 @@ import chalk from "chalk";
 class Logger {
 
 	protected mainColor: string;
+	protected successColor: string;
 	protected errorColor: string;
 
 	constructor() {
-		this.mainColor = "#d4fdd7";
+		this.mainColor = "#abf7a7";
+		this.successColor = "#41f45e";
 		this.errorColor = "#ee7474";
 	}
 
+	public write(...strs: string[]) {
+		console.log(strs.join(" "))
+	}
+
 	public log(...strs: string[]) {
-		this.write(strs.join(" "));
+		this.output(strs.join(" "));
 	}
 
 	public error(...strs: string[]) {
-		this.write(strs.join(" "), this.errorColor);
+		this.output(strs.join(" "), this.errorColor);
 	}
 
-	private write(str: string, color: string = this.mainColor) {
+	public success(...strs: string[]) {
+		this.output(strs.join(" "), this.successColor);
+	}
+
+	private output(str: string, color: string = this.mainColor) {
 		console.log(`${chalk.bgHex(color)(" ")} ${str.replace("\n", `\n${chalk.bgHex(color)(" ")} `)}`);
 	}
 
