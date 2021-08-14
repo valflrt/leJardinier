f03818f0dfbfb398b0ee939d61c91aae12752f07
valflrt <vfleurit@gmail.com>
2021-08-14 14:49:02 +0200

    created src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("worked");
+import chalk from "chalk";
+
+import LeJardinier from "./bot";
+import log from "./bot/log";
+
+log.logger.clear();
+
+log.logger.write(` ${chalk.hex("#abf7a7").bold`Le Jardinier`} 🍀 ${chalk.grey.italic`v2.1`}`);
+log.logger.write(chalk.grey`  by valflrt`);
+
+log.logger.newLine(2);
+
+LeJardinier.start();
