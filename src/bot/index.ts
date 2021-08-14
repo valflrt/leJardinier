3ab10109370da0da3c312c881751151c3f253338
valflrt <vfleurit@gmail.com>
2021-08-14 13:10:32 +0200

    created bot.ts

---
+++ src/bot/index.ts
@@ -0,0 +1,30 @@
+import Discord from "discord.js";
+import token from "../config/token";
+
+import log from "./log";
+
+class LeJardinier {
+
+	private bot: Discord.Client;
+
+	constructor() {
+		this.bot = new Discord.Client({ intents: [] });
+
+		this.bot.on("ready", () => this.onReady());
+		this.bot.on("messageCreate", (message) => this.onMessageCreate(message));
+
+		this.bot.login(token!);
+	}
+
+	private onReady() {
+		log.bot.connected(this.bot.user!.tag);
+	}
+
+	private onMessageCreate(message: Discord.Message) {
+		log.logger.write("new message");
+		log.bot.message(message);
+	}
+
+}
+
+export default new LeJardinier();
