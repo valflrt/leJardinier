16f268b35bbbd7bf9bda7159df9b79352b85e709
valflrt <vfleurit@gmail.com>
2021-08-14 13:27:18 +0200

    added comments

--- src/bot/log.ts
+++ src/bot/log.ts
@@ -13,15 +13,38 @@ class Logger {
 		this.errorColor = "#ee7474";
 	}
 
+	/**
+	 * general logging functions 
+	 * all these functions have the same structure
+	 * @param strs strings to log (joined with one space)
+	 */
 	public write = (...strs: string[]) => console.log(strs.join(" "));
 	public log = (...strs: string[]) => this.output(strs.join(" "));
 	public error = (...strs: string[]) => this.output(strs.join(" "), this.errorColor);
 	public success = (...strs: string[]) => this.output(strs.join(" "), this.successColor);
 
+	/**
+	 * clears the console
+	 */
 	public clear = () => console.clear();
+
+	/**
+	 * adds line breaks (specified number)
+	 * @param number number of line breaks
+	 * @author valflrt
+	 */
 	public newLine = (number: number = 1) => this.write(`\n`.repeat(number - 1));
+
+	/**
+	 * logs time
+	 */
 	public time = () => this.write(new Date().toLocaleTimeString());
 
+	/**
+	 * 
+	 * @param str string to log
+	 * @param color color of the "color block" at the str beginning
+	 */
 	private output = (str: string, color: string = this.mainColor) =>
 		console.log(`${chalk.bgHex(color)(" ")} ${str.replace("\n", `\n${chalk.bgHex(color)(" ")} `)}`);
 
@@ -33,8 +56,18 @@ class BotLogger extends Logger {
 		super();
 	}
 
-	public message = (messageObject: Discord.Message) => this.log(messageObject.content, messageObject.author.tag);
+	/**
+	 * logs every message (username#0000: message content)
+	 * @param messageObject discord message object
+	 * @author valflrt
+	 */
+	public message = (messageObject: Discord.Message) => this.log(`${chalk.bold(messageObject.content)}: ${messageObject.author.tag}`);
 
+	/**
+	 * logs a success message when the bot connected
+	 * @param tag bot's tag (username#0000)
+	 * @author valflrt
+	 */
 	public connected = (tag: string) => {
 		this.success(`successfully logged in as ${chalk.underline(tag)}`);
 		this.newLine();
