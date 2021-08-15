82bb51f2729f9c46ee8f4dc93d16258527d38129
valflrt <vfleurit@gmail.com>
2021-08-15 19:27:11 +0200

    edited message.ts

--- src/bot/message.ts
+++ src/bot/message.ts
@@ -23,6 +23,14 @@ class MessageInfo {
 		this.embed = this.setEmbed();
 	};
 
+	private getCommand = (): Command => {
+		return commands.toArray().find(command => message.content.match(new RegExp(`^${config.prefix}${command.name}`, "g")) !== null)[0];
+	}
+
+	private setEmbed = () => {
+		return;
+	}
+
 };
 
 export default MessageInfo;
