e2813954e33aeb2259403c0d865cd557da5be4b4
valflrt <vfleurit@gmail.com>
2021-08-19 11:48:52 +0200

    fixed methods

--- src/bot/message.ts
+++ src/bot/message.ts
@@ -1,7 +1,7 @@
 import { Client, MessageEmbed } from "discord.js";
 
 import commands from "../commands/commands";
-import ReplyMethods from "./message.methods";
+import ReplyMethods from "./methods";
 import { Command, TMessage } from "../types";
 
 class MessageInfo {
@@ -24,18 +24,10 @@ class MessageInfo {
 
 	private getCommand = () => commands.find(this.message.content);
 
-	private setEmbed = () => new MessageEmbed()
-		.setColor("#49a013")
+	private setEmbed = (): MessageEmbed => new MessageEmbed()
+		.setAuthor(this.bot.user!.username, "https://media.discordapp.net/attachments/749765499998437489/823241819801780254/36fb6d778b4d4a108ddcdefb964b3cc0.webp")
 		.setFooter(`${new Date().toLocaleString()} - Answering ${this.message.author.tag} - ${this.command.name}`)
-		.setAuthor(this.bot.user!.username, "https://media.discordapp.net/attachments/749765499998437489/823241819801780254/36fb6d778b4d4a108ddcdefb964b3cc0.webp");
-
-	private getCommand = (): Command => {
-		return commands.toArray().find(command => message.content.match(new RegExp(`^${config.prefix}${command.name}`, "g")) !== null)[0];
-	}
-
-	private setEmbed = () => {
-		return;
-	}
+		.setColor("#49a013");
 
 };
 
