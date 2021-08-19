31e5eb138b80d28c916aa4d3835fda31b9cfa8c7
valflrt <vfleurit@gmail.com>
2021-08-19 11:49:57 +0200

    fixed command hey

--- src/commands/hey.ts
+++ src/commands/hey.ts
@@ -1,17 +1,15 @@
-import commands from ".";
 import { Command } from "../types";
 import MessageInfo from "../bot/message";
+import * as utils from "../utils"
 
 const hey: Command = {
 	name: "hey",
 	description: "Greet the bot",
 	syntax: `hey`,
 	execute: (messageInfo: MessageInfo) => {
-		let { message } = messageInfo;
-		message.embed(`${utils.randomItem("hey", "Hii", "Yo")} ${message.author} ${utils.randomItem(":3", ":)", "!")}`);
+		let { methods, message } = messageInfo;
+		methods.embed(`${utils.randomItem("hey", "Hii", "Yo")} ${message.author} ${utils.randomItem(":3", ":)", "!")}`);
 	}
-});
-
-commands.add(hey);
+};
 
 export default hey;
