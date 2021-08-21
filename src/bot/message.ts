3316d5a0e775a6264e3d6c335dca65ee8334650d
valflrt <vfleurit@gmail.com>
2021-08-21 09:05:06 +0200

    some small fixes

--- src/bot/message.ts
+++ src/bot/message.ts
@@ -1,8 +1,9 @@
 import { Client, Message, MessageEmbed } from "discord.js";
 
 import commands from "../commands/index";
-import ReplyMethods from "./methods";
 import { Command } from "../types";
+import ReplyMethods from "./methods";
+import config from "../config";
 
 class MessageInstance {
 
@@ -17,21 +18,22 @@ class MessageInstance {
 	constructor(message: Message, bot: Client) {
 		this.message = message;
 		this.bot = bot;
-		this.methods = new ReplyMethods(this);
 
 		this.command = this.getCommand();
 		this.hasCommand = this.command ? true : false;
-		this.embed = this.setEmbed();
+		this.embed = this.generateEmbed();
+
+		this.methods = new ReplyMethods(this);
 	}
 
-	private getCommand = (): Command => {
+	private getCommand = (): Command | undefined => {
 		let command = commands.find(this.message.content);
 		return command;
 	};
 
-	private setEmbed = (): MessageEmbed => new MessageEmbed()
+	private generateEmbed = (): MessageEmbed => new MessageEmbed()
 		.setAuthor(this.bot.user!.username, "https://media.discordapp.net/attachments/749765499998437489/823241819801780254/36fb6d778b4d4a108ddcdefb964b3cc0.webp")
-		.setFooter(`${new Date().toLocaleString()} - Answering ${this.message.author.tag}${this.command ? `- ${this.command.name}` : ""} `)
+		.setFooter(`${new Date().toLocaleString()} - Answering ${this.message.author.tag}${this.command ? ` - ${config.prefix}${this.command.name}` : ""} `)
 		.setColor("#49a013");
 
 };
