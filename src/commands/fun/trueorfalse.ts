6add03a3392d29d853238aa917be1d52ffa109df
valflrt <vfleurit@gmail.com>
2021-08-25 12:27:03 +0200

    edited trueorfalse

--- src/commands/fun/trueorfalse.ts
+++ src/commands/fun/trueorfalse.ts
@@ -4,9 +4,9 @@ import { Command } from "../../types";
 import * as utils from "../../utils"
 
 const trueorfalse: Command = {
-	name: "trueorfalse",
-	description: "Answer \"true\" or \"false\" randomly",
-	syntax: `true or false <?sentence>`,
+	name: "t/f",
+	description: `Answer "true" or "false" randomly`,
+	syntax: `true or false [?sentence]`,
 	execution: (messageInstance: MessageInstance) => {
 		let { methods, message, bot, commandArgs } = messageInstance;
 		methods.sendEmbed(`${commandArgs && `${ReplyMethods.mention(message.author.id)}\n${commandArgs}\n${ReplyMethods.mention(bot.user!.id)}\n`}${utils.randomItem("true !", "false !")}`)
