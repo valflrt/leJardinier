75e38c33e7aac0c7b25a9460286fc0de70089abc
valflrt <vfleurit@gmail.com>
2021-08-31 18:26:44 +0200

    added command percentage in command/index.ts

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -6,10 +6,12 @@ import help from "./utility/help";
 // fun commands
 import hey from "./fun/hey";
 import trueorfalse from "./fun/trueorfalse";
+import percentage from "./fun/percentage";
 
 // exporting commands object (commands are ordered here)
 export default new Commands(
 	help,
 	hey,
-	trueorfalse
+	trueorfalse,
+	percentage
 )
