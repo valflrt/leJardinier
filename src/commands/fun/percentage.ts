c90f4cd96be75ec4eec915a65340994efffc17d6
valflrt <vfleurit@gmail.com>
2021-08-31 18:33:32 +0200

    fixed percentage.ts

--- src/commands/fun/percentage.ts
+++ src/commands/fun/percentage.ts
@@ -5,11 +5,11 @@ import * as utils from "../../utils"
 
 const percentage: Command = {
 	name: "percentage",
-	description: "Greet the bot",
+	description: "Gives a random percentage",
 	syntax: `percentage [?sentence]`,
 	execution: (messageInstance: MessageInstance) => {
 		let { methods, message, bot, commandArgs } = messageInstance;
-		methods.sendEmbed(`${commandArgs && `${ReplyMethods.mention(message.author.id)}\n${commandArgs}\n${ReplyMethods.mention(bot.user!.id)}\n`}${utils.oneOf(100) ? utils.randomNumber(100, 300) : utils.randomItem(0, 100)}%`)
+		methods.sendEmbed(`${commandArgs && `${ReplyMethods.mention(message.author.id)}\n${commandArgs}\n${ReplyMethods.mention(bot.user!.id)}\n`}${utils.oneOf(100) ? utils.randomNumber(100, 300) : utils.randomNumber(0, 100)}%`)
 	}
 }
 
