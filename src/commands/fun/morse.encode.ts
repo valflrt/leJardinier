d957637a07e0d7fa4119fb390492e0416dc01c73
valflrt <vfleurit@gmail.com>
2021-09-01 16:01:17 +0200

    added "morse encode" command

---
+++ src/commands/fun/morse.encode.ts
@@ -0,0 +1,30 @@
+import MessageInstance from "../../bot/message";
+import { Command } from "../../types";
+
+import morseTable from "../../assets/morse.table";
+
+const morseEncode: Command = {
+	name: "morse encode",
+	description: `Encode text to Morse code`,
+	syntax: `morse encode [sentence]`,
+	execution: (messageInstance: MessageInstance) => {
+		let { methods, commandArgs } = messageInstance;
+
+		const encode = (text: string, morse: string[] = []): string => {
+
+			let char = text.substr(0, 1);
+
+			let morseLetter = morseTable.find(item => item[0] === char);
+
+			if (morseLetter) morse.push(morseLetter[1]);
+			else if (char === " ") morse.push("|");
+
+			if (text.length === 0) return morse.reverse().join("  ");
+			else return encode(text.slice(1), morse);
+		};
+
+		methods.sendEmbed(commandArgs ? encode(commandArgs?.replace(/[^abcdefghijklmopqrstuvwxyz\s]/g, "")) : "You need to give some text to convert to morse...");
+	}
+}
+
+export default morseEncode;
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -7,11 +7,13 @@ import help from "./utility/help";
 import hey from "./fun/hey";
 import trueorfalse from "./fun/trueorfalse";
 import percentage from "./fun/percentage";
+import morseEncode from "./fun/morse.encode";
 
 // exporting commands object (commands are ordered here)
 export default new Commands(
 	help,
 	hey,
 	trueorfalse,
-	percentage
+	percentage,
+	morseEncode
 )
