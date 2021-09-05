9da2597e452ec5fc199583f06a1a97c5968fead3
valflrt <vfleurit@gmail.com>
2021-09-05 18:31:37 +0200

    renamed CommandType to ICommand

--- src/bot/command.ts
+++ src/bot/command.ts
@@ -1,24 +1,27 @@
 import config from "../config";
 
-export interface CommandType {
+export interface ICommand {
 	name: string,
 	description: string,
 	syntax: string,
-	execution: Function
+	execution: Function,
+	subcommands?: ICommand[]
 }
 
-export class Command implements CommandType {
+export class Command implements ICommand {
 
 	name: string;
 	description: string;
 	syntax: string;
 	execution: Function;
+	subcommands?: ICommand[]
 
-	constructor(command: CommandType) {
+	constructor(command: ICommand) {
 		this.name = command.name;
 		this.description = command.description;
 		this.syntax = `${config.prefix}${command.syntax}`;
 		this.execution = command.execution;
+		this.subcommands = command?.subcommands;
 	}
 
 }
