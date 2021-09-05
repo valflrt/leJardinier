d448d98b53e04a0c77700bc81058bb66637ace78
valflrt <vfleurit@gmail.com>
2021-09-05 19:16:12 +0200

    specified categories

--- src/commands/fun/hey.ts
+++ src/commands/fun/hey.ts
@@ -7,6 +7,7 @@ const hey = new Command({
 	name: "hey",
 	description: "Greet the bot",
 	syntax: `hey`,
+	category: { name: "Fun", order: 1 },
 	execution: (messageInstance: MessageInstance) => {
 		let { methods, message } = messageInstance;
 		methods.sendEmbed(`${utils.randomItem("Hey", "Hii", "Heyaa", "Yo")} ${ReplyMethods.mention(message.author.id)} ${utils.randomItem(":3", ":)", "!")}`);
--- src/commands/fun/percentage.ts
+++ src/commands/fun/percentage.ts
@@ -7,6 +7,7 @@ const percentage = new Command({
 	name: "percentage",
 	description: "Gives a random percentage",
 	syntax: `percentage [?sentence]`,
+	category: { name: "Fun", order: 1 },
 	execution: (messageInstance: MessageInstance) => {
 		let { methods, message, bot, commandArgs } = messageInstance;
 		methods.sendEmbed(`${commandArgs && `${ReplyMethods.mention(message.author.id)}\n${commandArgs}\n${ReplyMethods.mention(bot.user!.id)}\n`}${utils.oneOf(100) ? utils.randomNumber(100, 300) : utils.randomNumber(0, 100)}%`)
