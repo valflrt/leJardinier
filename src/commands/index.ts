3dd3c823f3c87aa723e05dd0acd3a50dd1bdf7ba
valflrt <vfleurit@gmail.com>
2021-09-09 11:13:41 +0200

    added command invite

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,6 +2,7 @@ import CommandList from "../bot/command.list";
 
 // utility commands
 import help from "./utility/help";
+import invite from "./utility/invite";
 
 // fun commands
 import hey from "./fun/hey";
@@ -12,6 +13,7 @@ import morse from "./fun/morse";
 // exporting commands object
 let commandList = new CommandList(
 	help,
+	invite,
 	hey,
 	trueorfalse,
 	percentage,
@@ -20,7 +22,10 @@ let commandList = new CommandList(
 
 // sets categories (so commands are ordered and sorted here)
 commandList
-	.setCategory("Utility", [help])
+	.setCategory("Utility", [
+		help,
+		invite
+	])
 	.setCategory("Fun", [
 		hey,
 		trueorfalse,
---
+++ src/commands/utility/invite.ts
@@ -0,0 +1,18 @@
+import { MessageEmbed } from "discord.js";
+
+import MessageInstance from "../../bot/message";
+import { Command } from "../../bot/command";
+
+const invite = new Command({
+	name: "invite",
+	description: "Get bot invitation link",
+	execution: (messageInstance: MessageInstance) => {
+		let { methods, bot } = messageInstance;
+		methods.sendCustomEmbed((embed: MessageEmbed) => embed
+			.setTitle("Invitation link")
+			.setURL(bot.generateInvite({ scopes: ["bot"], permissions: "ADMINISTRATOR" }))
+			.setDescription(`Click the click above to add me in your server`));
+	}
+})
+
+export default invite;
