1a9383614232370e9cbb0ee8bb7a8d55297d1cdc
valflrt <vfleurit@gmail.com>
2021-09-10 19:29:13 +0200

    removed lowdb and reseted files in src/database

--- src/database/schemas.ts
+++
@@ -1,9 +0,0 @@
-import { ClientUser, Guild } from "discord.js";
-
-export interface SDB {
-	guilds: Guild[]
-}
-
-export interface SUser extends ClientUser { };
-
-export interface SGuild extends Guild { };
