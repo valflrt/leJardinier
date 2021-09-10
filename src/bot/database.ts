13512c16bbf5a408fec8de981497fc6f0a092b72
valflrt <vfleurit@gmail.com>
2021-09-10 14:07:28 +0200

    fixed and added methods

--- src/bot/database.ts
+++ src/bot/database.ts
@@ -1,23 +1,50 @@
 import { Low } from "lowdb/lib";
-import database, { DBSchema } from "../database";
+
+import database from "../database";
+import { SDB, SGuild } from "../database/schemas";
 
 export default class Database {
 
-	private DB: Low<DBSchema>;
+	private DB: Low<SDB>;
 
 	constructor() {
 		this.DB = database;
-		this.read().catch(err => console.log(err));
+		this.update();
 	}
 
 	/**
-	 * Reads database
+	 * Adds a guild to database
+	 * @param guild {SGuild} guild to add
 	 * @returns {Promise} promise object
 	 */
-	private read = (): Promise<any> => this.DB.read();
+	public addGuild = (guild: SGuild): Promise<any> => {
+		this.update();
+		this.DB.data
+			?.guilds
+			.push(guild);
+		return this.save();
+	}
 
 	/**
-	 * saves updated data to database
+	 * Gets a guild by id
+	 * @param id id to look for
+	 * @returns {SGuild}
+	 */
+	public getGuild = (id: string): SGuild | undefined => {
+		this.update();
+		return this.DB.data
+			?.guilds
+			.find(guild => guild.id === id);
+	}
+
+	/**
+	 * Updates database instance
+	 * @returns {Promise} promise object
+	 */
+	private update = (): Promise<any> => this.DB.read();
+
+	/**
+	 * Saves updated data to database
 	 * @returns {Promise} promise object
 	 */
 	private save = (): Promise<any> => this.DB.write();
