2b77b38fd9ec1d5c6e1e90bdb1ef15e911be8271
valflrt <vfleurit@gmail.com>
2021-09-10 20:07:05 +0200

    edited bot/database.ts

--- src/bot/database.ts
+++ src/bot/database.ts
@@ -1,53 +1,23 @@
-import { Low } from "lowdb/lib";
+import { MongoClient, Collection } from "mongodb";
 
-import database from "../database";
-import { SDB, SGuild } from "../database/schemas";
+import config from "../config/database";
 
 class Database {
 
-	private DB: Low<SDB>;
+	private client: MongoClient;
+	private DB: Collection<Document> | undefined;
 
 	constructor() {
-		this.DB = database;
-		this.update();
+		this.client = new MongoClient(config.databaseURI);
+		this.connect();
 	}
 
-	/**
-	 * Adds a guild to database
-	 * @param guild {SGuild} guild to add
-	 * @returns {Promise} promise object
-	 */
-	public addGuild = (guild: SGuild): Promise<any> => {
-		this.update();
-		this.DB.data
-			?.guilds
-			.push(guild);
-		return this.save();
+	private connect = async () => {
+		let connection = await this.client.connect();
+		this.DB = connection
+			.db("lejardinier")
+			.collection("main");
 	}
-
-	/**
-	 * Gets a guild by id
-	 * @param id id to look for
-	 * @returns {SGuild}
-	 */
-	public getGuild = (id: string): SGuild | undefined => {
-		this.update();
-		return this.DB.data
-			?.guilds
-			.find(guild => guild.id === id);
-	}
-
-	/**
-	 * Updates database instance
-	 * @returns {Promise} promise object
-	 */
-	private update = (): Promise<any> => this.DB.read();
-
-	/**
-	 * Saves updated data to database
-	 * @returns {Promise} promise object
-	 */
-	private save = (): Promise<any> => this.DB.write();
 
 }
 
