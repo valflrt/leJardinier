08605f5b4fd0cfbb9b5498ea9c1411590dcebf34
valflrt <vfleurit@gmail.com>
2021-09-12 16:16:50 +0200

    fixed method connect

--- src/bot/database.ts
+++ src/bot/database.ts
@@ -9,16 +9,20 @@ class Database {
 
 	constructor() {
 		this.client = new MongoClient(config.databaseURI);
-		this.connect();
 	}
 
-	private connect = async () => {
-		let connection = await this.client.connect();
-		this.DB = connection
-			.db("lejardinier")
-			.collection("main");
-	}
+	public connect = () => new Promise<void>(async (resolve) => {
+		try {
+			let connection = await this.client.connect();
+
+			this.DB = connection
+				.db("lejardinier")
+				.collection("main");
+
+			resolve();
+		} catch (e) { console.log(e) };
+	})
 
 }
 
-export default new Database;
+export default new Database();
