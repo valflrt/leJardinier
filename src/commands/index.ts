e6da566fa2db7e2cfab968d25c16179fc8ce5162
valflrt <vfleurit@gmail.com>
2021-09-12 16:18:17 +0200

    removed shit that shouldn't have been here

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -9,9 +9,6 @@ import hey from "./fun/hey";
 import trueorfalse from "./fun/trueorfalse";
 import percentage from "./fun/percentage";
 import morse from "./fun/morse";
-import { Command } from "../bot/command";
-import database from "../bot/database";
-import MessageInstance from "../bot/message";
 
 // exporting commands object
 let commandList = new CommandList(
@@ -20,15 +17,7 @@ let commandList = new CommandList(
 	hey,
 	trueorfalse,
 	percentage,
-	morse,
-	new Command({
-		name: "registerguild",
-		description: "register a guild",
-		execution: (messageInstance: MessageInstance) => {
-			let { message } = messageInstance;
-			database.addGuild(message.guild!);
-		}
-	})
+	morse
 )
 
 // sets categories (so commands are ordered and sorted here)
