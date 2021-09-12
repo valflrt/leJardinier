3fcb8c333acef9ebd488e70e55a86feece1ff74a
valflrt <vfleurit@gmail.com>
2021-09-12 17:53:30 +0200

    added user model

---
+++ src/database/models/user.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export interface IUserSchema {
+	id: string,
+	username: string
+}
+
+export const UserSchema = new mongoose.Schema<IUserSchema>({
+	id: { type: String, required: true },
+	username: { type: String, required: true }
+})
+
+export const UserModel = mongoose.model<IUserSchema>("Guild", UserSchema);
