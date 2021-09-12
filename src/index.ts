8d4cdb1daae32f3f0e3a16a455670e6dc3ed0b3d
valflrt <vfleurit@gmail.com>
2021-09-12 16:17:05 +0200

    added async start

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import chalk from "chalk";
 
 import LeJardinier from "./bot/bot";
+import database from "./bot/database";
 import log from "./bot/log";
 
 log.logger.clear();
@@ -8,6 +9,16 @@ log.logger.clear();
 log.logger.write(` ${chalk.hex("#abf7a7").bold`Le Jardinier`} 🍀 ${chalk.grey.italic`v2.1`}`);
 log.logger.write(chalk.grey`  by valflrt`);
 
-log.logger.newLine(2);
+async () => {
 
-LeJardinier.start();
+	try {
+
+		await database.connect();
+
+	} finally {
+		log.logger.newLine(2);
+
+		LeJardinier.start();
+	}
+
+}
