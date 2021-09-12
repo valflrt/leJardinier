5ba112263e7059c9ab730dbc6f718650450f8487
valflrt <vfleurit@gmail.com>
2021-09-12 17:57:02 +0200

    changed export, added GuildManager, ...

--- src/bot/database.ts
+++ src/bot/database.ts
@@ -2,16 +2,48 @@ import mongoose from "mongoose";
 
 import config from "../config/database";
 
-import guild from "../database/schemas/guild";
+import { IGuildSchema, GuildModel } from "../database/models/guild";
+import { IUserSchema, UserModel } from "../database/models/user";
 
-class Database {
-
-	public connect = () => mongoose.connect(config.databaseURI);
+class GuildManager {
 
 	public findById = async (id: string) => {
-		return await guild.findOne({ id: id })
+		return await GuildModel.findOne({ id });
+	}
+
+	public removeById = async (id: string) => {
+		return await GuildModel.findOneAndRemove({ id });
+	}
+
+	public add = async (guild: IGuildSchema) => {
+		await new GuildModel(guild).save();
 	}
 
 }
 
-export default new Database();
+class UserManager {
+
+	public findById = async (id: string) => {
+		return await UserModel.findOne({ id });
+	}
+
+	public removeById = async (id: string) => {
+		return await UserModel.findOneAndRemove({ id });
+	}
+
+	public add = async (user: IUserSchema) => {
+		await new UserModel(user).save();
+	}
+
+}
+
+export const guildManager = new GuildManager();
+export const userManager = new UserManager();
+
+export const connect = () => mongoose.connect(config.databaseURI);
+
+export default {
+	connect,
+	guildManager,
+	userManager
+}
