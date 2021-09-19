8bb447e9e1493ce2753def25c0f82f26fb848dde
valflrt <vfleurit@gmail.com>
2021-09-19 17:39:47 +0200

    added "nose touching" in uncommon.ts

--- src/bot/uncommon.ts
+++ src/bot/uncommon.ts
@@ -1,4 +1,18 @@
 /**
- * will be used when bot needs to do something that
+ * will be used when the bot needs to do something that
  * can't be done with common functions and features
 */
+
+import { Client, TextChannel } from "discord.js";
+
+export default {
+	start: (bot: Client) => {
+		setInterval(() => {
+			let time = new Date();
+			if (time.getHours() === time.getMinutes()) {
+				let channel = bot.channels.cache.get("802634814771560518") as TextChannel;
+				channel.send("<@&802634842557644900> Hurry up !")
+			}
+		}, 5000);
+	}
+}
