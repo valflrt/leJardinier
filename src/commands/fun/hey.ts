e01e6232614ce8e583921db2d819154f255a834c
valflrt <vfleurit@gmail.com>
2021-09-22 16:35:49 +0200

    implemented "reactions"

--- src/commands/fun/hey.ts
+++ src/commands/fun/hey.ts
@@ -2,13 +2,15 @@ import MessageInstance from "../../bot/message";
 import { Command } from "../../bot/command";
 import * as utils from "../../utils"
 
+import reactions from "../../assets/reactions";
+
 const hey = new Command({
 	name: "hey",
 	description: "Greet the bot",
 	execution: (messageInstance: MessageInstance) => {
 		let { methods, message } = messageInstance;
 		methods.sendEmbed(`${utils.randomItem("Hey", "Hii", "Heyaa", "Yo")} `
-			.concat(`${message.author.toString()} ${utils.randomItem(":3", ":)", "!")}`)
+			.concat(`${message.author.toString()} ${reactions.smile.random()}`)
 		);
 	}
 })
