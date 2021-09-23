fbf4dd244bf19a311162bc656cc0793597ea6618
valflrt <vfleurit@gmail.com>
2021-09-23 19:15:14 +0200

    added interactions.ts added "button link"

---
+++ src/bot/interactions.ts
@@ -0,0 +1,9 @@
+import { MessageActionRow, MessageButton } from "discord.js"
+
+export const linkButton = (label: string, url: string) =>
+	new MessageActionRow()
+		.addComponents(
+			new MessageButton()
+				.setLabel(label)
+				.setURL(url)
+		);
