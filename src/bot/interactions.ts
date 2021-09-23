4a1732dc135f8c1b1af61c6582d5e74674420161
valflrt <vfleurit@gmail.com>
2021-09-23 19:21:08 +0200

    fixed interactions.ts

--- src/bot/interactions.ts
+++ src/bot/interactions.ts
@@ -2,5 +2,5 @@ import { MessageActionRow, MessageButton } from "discord.js";
 
 export const linkButton = (label: string, url: string) =>
 	new MessageActionRow().addComponents(
-		new MessageButton().setLabel(label).setURL(url)
+		new MessageButton().setStyle("LINK").setLabel(label).setURL(url)
 	);
