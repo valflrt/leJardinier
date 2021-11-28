6371eb1f8f7222aef76c3c41765e51a20dc49d92
valflrt <vfleurit@gmail.com>
2021-11-28 15:44:27 +0100

    added comments and property "required"

--- src/lib/commandManager/classes/commandParameter.ts
+++ src/lib/commandManager/classes/commandParameter.ts
@@ -1,27 +1,45 @@
 export default class CCommandParameter {
-  private _name!: string;
-  private _type!: StringConstructor | NumberConstructor;
+	private _name!: string;
+	private _type!: StringConstructor | NumberConstructor;
+	private _required: boolean = false;
 
-  public setName(name: string) {
-    this.name = name;
-    return this;
-  }
-  public setType(type: StringConstructor | NumberConstructor) {
-    this.type = type;
-    return this;
-  }
+	/**
+	 * sets parameter name
+	 * @param name parameter name
+	 */
+	public setName(name: string): this {
+		this.name = name;
+		return this;
+	}
+	public setType(type: StringConstructor | NumberConstructor): this {
+		this.type = type;
+		return this;
+	}
+	public setRequired(required: boolean): this {
+		this.required = required;
+		return this;
+	}
 
-  public set name(v: string) {
-    this._name = v.toLowerCase().trim();
-  }
-  public get name(): string {
-    return this._name;
-  }
+	// setters and getters
 
-  public set type(v: StringConstructor | NumberConstructor) {
-    this._type = v;
-  }
-  public get type(): StringConstructor | NumberConstructor {
-    return this._type;
-  }
+	public set name(v: string) {
+		this._name = v.toLowerCase().trim();
+	}
+	public get name(): string {
+		return this._name;
+	}
+
+	public set type(v: StringConstructor | NumberConstructor) {
+		this._type = v;
+	}
+	public get type(): StringConstructor | NumberConstructor {
+		return this._type;
+	}
+
+	public set required(v: boolean) {
+		this._required = v;
+	}
+	public get required(): boolean {
+		return this._required;
+	}
 }
