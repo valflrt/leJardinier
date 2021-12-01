86625267dc4990439063a439a42dee82d6daeaeb
valflrt <vfleurit@gmail.com>
2021-12-01 17:51:37 +0100

    fixed prefix problem and edited a regex

--- src/lib/commandManager/classes/messageContent.ts
+++ src/lib/commandManager/classes/messageContent.ts
@@ -1,16 +1,23 @@
+import config from "../../../config";
+
 export default class CMessageContent {
 	public readonly commandPattern: string[] = [];
 	public readonly parameters: string[] = [];
 
 	constructor(messageText: string) {
-		let prefix = "lj!";
-		if (messageText.startsWith(prefix)) {
+		if (messageText.startsWith(config.prefix)) {
 			this.commandPattern = messageText
-				.replace(new RegExp(`(^${prefix})|( .{0,}$)`, "g"), "")
+				.replace(new RegExp(`(^${config.prefix})|( .{0,}$)`, "g"), "")
 				.trim()
 				.split(/(?!^)\.(?!$)/g);
 			this.parameters = messageText
-				.replace(new RegExp(`^(${prefix}\\w+(\\.\\w+){0,} )`, "g"), "")
+				.replace(
+					new RegExp(
+						`^((?:${config.prefix}\\w+)(\\.\\w{0,}){0,} {0,})`,
+						"g"
+					),
+					""
+				)
 				.trim()
 				.split(/\s+/g);
 		}
