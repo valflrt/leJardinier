25510606b6e4ee618c7feeaa1ceecc9b5fe1b542
valflrt <vfleurit@gmail.com>
2021-12-04 10:46:53 +0100

    improved command "reverse"

--- src/commands/fun/reverse.ts
+++ src/commands/fun/reverse.ts
@@ -2,23 +2,23 @@ import { inlineCode } from "@discordjs/builders";
 
 import CCommand from "../../lib/commandManager/classes/command";
 
-import reactions from "../../assets/reactions";
-
 const reverse = new CCommand()
 	.setName("reverse")
 	.setDescription("Reverses the specified text")
 	.setExecution(async (messageInstance) => {
 		let { methods, commandParameters } = messageInstance;
-		const reverseText = (text: string, acc: string[] = []): string => {
-			let char = text.charAt(0);
-			acc.unshift(char);
-			if (text.length !== 0)
-				return reverseText(text.substring(1, text.length), acc);
-			else return acc.join("");
-		};
+		const reverseText = (v: string): string[] =>
+			v.length === 0 ? [] : reverseText(v.slice(1)).concat(v[0]);
+		//(text: string, acc: string[] = []): string => {
+		// 	let char = text.charAt(0);
+		// 	acc.unshift(char);
+		// 	if (text.length !== 0)
+		// 		return reverseText(text.substring(1, text.length), acc);
+		// 	else return acc.join("");
+		// };
 		methods.sendTextEmbed(
-			`Here is you reversed text ${reactions.smile.random()}\n`.concat(
-				inlineCode(reverseText(commandParameters))
+			`Here is you reversed text:\n`.concat(
+				inlineCode(reverseText(commandParameters).join(""))
 			)
 		);
 	});
