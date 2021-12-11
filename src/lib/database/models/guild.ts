9a2794dba5e59fa3e4f21bbad9d4c33c0cf6ae99
valflrt <vfleurit@gmail.com>
2021-12-11 16:15:36 +0100

    edited playlist and guild schemas careful: command "music" will not work until fixed

--- src/lib/database/models/guild.ts
+++ src/lib/database/models/guild.ts
@@ -1,13 +1,18 @@
 import mongoose from "mongoose";
+import { IPlaylistSchema, PlaylistSchema } from "./playlist";
 
 export interface IGuildSchema {
 	id: string;
 	name: string;
+	playlist: IPlaylistSchema;
+	autoroleMessage: string;
 }
 
 export const GuildSchema = new mongoose.Schema<IGuildSchema>({
 	id: { type: String, required: true },
 	name: { type: String, required: true },
+	playlist: { type: PlaylistSchema },
+	autoroleMessage: { type: String },
 });
 
 export const GuildModel = mongoose.model<IGuildSchema>("Guild", GuildSchema);
--- src/lib/database/models/playlist.ts
+++ src/lib/database/models/playlist.ts
@@ -2,19 +2,12 @@ import mongoose from "mongoose";
 import { MoreVideoDetails } from "ytdl-core";
 
 export interface IPlaylistSchema {
-	guildId: string;
 	songs?: MoreVideoDetails[];
 }
 
 export const PlaylistSchema = new mongoose.Schema<IPlaylistSchema>({
-	guildId: { type: String, required: true },
 	songs: {
 		type: [Object],
 		default: [],
 	},
 });
-
-export const PlaylistModel = mongoose.model<IPlaylistSchema>(
-	"Playlist",
-	PlaylistSchema
-);
