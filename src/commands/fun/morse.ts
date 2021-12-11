4ee52ee39b4513b86ae288fed528e2ed696a7842
valflrt <vfleurit@gmail.com>
2021-12-11 14:37:53 +0100

    added command "morse.decode"

--- src/commands/fun/morse.ts
+++ src/commands/fun/morse.ts
@@ -24,7 +24,7 @@ const morse = new CCommand()
 	.addSubcommand((c) =>
 		c
 			.setName("encode")
-			.setDescription("Encode text to Morse code")
+			.setDescription("Encodes text to Morse code")
 			.addParameter((p) => p.setName("sentence").setRequired(true))
 			.setExecution(async (messageInstance) => {
 				let { methods, commandParameters } = messageInstance;
@@ -39,20 +39,71 @@ const morse = new CCommand()
 					);
 					let morseLetter = !tableItem ? "?" : tableItem[1];
 
-					if (char === " ") morse.push(" ");
-					else morse.push(morseLetter);
+					morse.push(morseLetter);
 
 					return encode(text.slice(1), morse);
 				};
 
 				if (commandParameters.length === 0)
 					methods.sendTextEmbed(
-						"You need to give some text to convert to morse..."
+						"You need to give some text to convert to Morse Code..."
 					);
 				else
 					methods.sendTextEmbed(
-						"Here is your morse encoded text:".concat(
-							codeBlock(encode(commandParameters.toLowerCase()))
+						"Here is your Morse encoded text:".concat(
+							codeBlock(
+								commandParameters
+									.split(/\s+/g)
+									.map((w) => encode(w))
+									.join(" / ")
+									.toLowerCase()
+							)
+						)
+					);
+			})
+			.addHelpCommand()
+	)
+	.addSubcommand((c) =>
+		c
+			.setName("decode")
+			.setDescription("Decodes Morse code")
+			.addParameter((p) => p.setName("morse code").setRequired(true))
+			.setExecution(async (messageInstance) => {
+				let { methods, commandParameters } = messageInstance;
+
+				console.log(commandParameters);
+
+				const decode = (
+					chars: string[],
+					decoded: string[] = []
+				): string => {
+					if (chars.length === 0) return decoded.join("");
+
+					let char = chars[0];
+
+					let tableItem = morseTable.find((item) => item[1] === char);
+					let letter = !tableItem ? "?" : tableItem[2];
+
+					decoded.push(letter);
+
+					return decode(chars.slice(1), decoded);
+				};
+
+				if (commandParameters.length === 0)
+					methods.sendTextEmbed(
+						"You need to give some Morse to decode..."
+					);
+				else
+					methods.sendTextEmbed(
+						"Here is your decoded text:".concat(
+							codeBlock(
+								commandParameters
+									.split(/\s\/\s/g)
+									.map((w) => decode(w.split(/\s/g)))
+									.join(" ")
+									.toLowerCase()
+									.trim()
+							)
 						)
 					);
 			})
