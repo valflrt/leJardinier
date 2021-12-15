d3904381f6c33a9ed24c7c6206858e84d2c6a2be
valflrt <vfleurit@gmail.com>
2021-12-15 18:36:10 +0100

    edited database schema "user"

--- src/lib/database/models/user.ts
+++ src/lib/database/models/user.ts
@@ -3,12 +3,10 @@ import { IStatSchema, StatSchema } from "./stat";
 
 export interface IUserSchema {
 	id: string;
-	username: string;
 	stats: IStatSchema;
 }
 
 export const UserSchema = new mongoose.Schema<IUserSchema>({
 	id: { type: String, required: true },
-	username: { type: String, required: true },
 	stats: { type: StatSchema },
 });
