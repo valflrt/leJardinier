ff5fc797d468befb96c0a2a5c62bc07ea937c055
valflrt <vfleurit@gmail.com>
2021-12-18 12:44:02 +0100

    renamed managers to database

--- src/managers/database/database.ts
+++ src/managers/database/database.ts
@@ -1,12 +1,12 @@
 import { MongoClient } from "mongodb";
 
-import managers from "./managers";
+import database from ".";
 import config from "../../config";
 
 const buildDatabase = async (): Promise<void> => {
 	const client = new MongoClient(config.secrets.databaseURI);
 	await client.connect();
-	managers.associate(client.db("lejardinier"));
+	database.associate(client.db("lejardinier"));
 };
 
 export default buildDatabase;
