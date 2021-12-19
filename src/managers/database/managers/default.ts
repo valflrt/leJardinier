89de4f1387ef3a77ac8b0483ca1271ae2a0997af
valflrt <vfleurit@gmail.com>
2021-12-19 15:57:28 +0100

    added method updateOrCreateOne added and edited comments

--- src/managers/database/managers/default.ts
+++ src/managers/database/managers/default.ts
@@ -38,10 +38,9 @@ export default class DefaultManager<Schema> {
 	}
 
 	/**
-	 * finds one object in the database using a filter based on the default
-	 * specified schema
-	 * @param filter typed filter
-	 * @param options look up options
+	 * finds one entry
+	 * @param filter filter to find the entries
+	 * @param options search options
 	 */
 	public async findOne(
 		filter: Partial<Schema>,
@@ -51,6 +50,11 @@ export default class DefaultManager<Schema> {
 		return doc ? (doc as unknown as Schema) : null;
 	}
 
+	/**
+	 * finds multiples entries
+	 * @param filter filter to find the entries
+	 * @param options search options
+	 */
 	public async findMany(
 		filter: Partial<Schema>,
 		options: FindOptions = {}
@@ -59,6 +63,11 @@ export default class DefaultManager<Schema> {
 		return docs.length !== 0 ? (docs as unknown[] as Schema[]) : null;
 	}
 
+	/**
+	 * creates one entry
+	 * @param doc entry to create
+	 * @param options creation options
+	 */
 	public createOne(
 		doc: Schema,
 		options: InsertOneOptions = {}
@@ -69,6 +78,11 @@ export default class DefaultManager<Schema> {
 		);
 	}
 
+	/**
+	 * creates multiple entries from an object array
+	 * @param docs array of entries to create
+	 * @param options creation options
+	 */
 	public createMany(
 		docs: Schema[],
 		options: BulkWriteOptions = {}
@@ -79,6 +93,11 @@ export default class DefaultManager<Schema> {
 		);
 	}
 
+	/**
+	 * finds and deletes one entry
+	 * @param filter filter to find the target entry
+	 * @param options delete options
+	 */
 	public deleteOne(
 		filter: Partial<Schema>,
 		options: FindOneAndDeleteOptions = {}
@@ -86,6 +105,12 @@ export default class DefaultManager<Schema> {
 		return this.collection.findOneAndDelete(filter, options);
 	}
 
+	/**
+	 * Finds and replace one entry
+	 * @param filter filter to find the target entry
+	 * @param replacement replacement to apply to the target entry
+	 * @param options replace options
+	 */
 	public replaceOne(
 		filter: Partial<Schema>,
 		replacement: Schema,
@@ -98,6 +123,12 @@ export default class DefaultManager<Schema> {
 		);
 	}
 
+	/**
+	 * updates an entry
+	 * @param filter filter to find the target entry
+	 * @param update update to apply to the target entry
+	 * @param options update options
+	 */
 	public updateOne(
 		filter: Partial<Schema>,
 		update: Partial<Schema>,
@@ -110,6 +141,31 @@ export default class DefaultManager<Schema> {
 		);
 	}
 
+	/**
+	 * updates an entry and if not found creates one
+	 * @param filter filter to find target entry
+	 * @param update update to apply to the target entry
+	 * @param doc if not found object to create the new entry with
+	 * @param options update options
+	 */
+	public async updateOrCreateOne(
+		filter: Partial<Schema>,
+		update: Partial<Schema>,
+		doc: Schema,
+		options: FindOneAndUpdateOptions = {}
+	): Promise<ModifyResult<unknown>> {
+		let foundEntry = await this.findOne(filter);
+		if (!foundEntry) await this.createOne(doc);
+		return this.collection.findOneAndUpdate(
+			filter,
+			{ $set: mix(new this.schemaConstructor(), update) },
+			options
+		);
+	}
+
+	/**
+	 * wether if the current manager is associated or not
+	 */
 	public get associated(): boolean {
 		return !!this.collection;
 	}
