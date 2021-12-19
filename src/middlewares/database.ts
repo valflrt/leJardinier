4ba4e01f1fac431bf2ffa48f04222691cd249e52
valflrt <vfleurit@gmail.com>
2021-12-19 15:33:41 +0100

    fixed middlewares/database.ts

--- src/middlewares/database.ts
+++ src/middlewares/database.ts
@@ -1,33 +1,72 @@
-import { Message } from "discord.js";
+import { inlineCode } from "@discordjs/builders";
+
+import MessageInstance from "../bot/message";
 
 import database from "../managers/database";
+import { randomItem } from "../utils";
 
-export const onMessage = async (message: Message) => {
-	let update = await database.users.updateStats(
-		message.guildId!,
-		message.author.id,
-		(currentStats) => {
-			/**
-			 * Math.floor(5 ** 1.1) is the first level cost
-			 * it increments with every level increase
-			 */
-			let currentLevelXPCost = Math.floor(5 ** 1.1 * currentStats.level!);
-			let hasLeveledUp = currentStats.xp! + 1 === currentLevelXPCost;
-			return {
-				messageCount: currentStats.messageCount! + 1,
-				xp: hasLeveledUp ? 0 : currentStats.xp! + 1,
-				level: hasLeveledUp
-					? currentStats.level! + 1
-					: currentStats.level!,
-			};
+import reactions from "../assets/reactions";
+
+const onMessage = async (messageInstance: MessageInstance) => {
+	let { methods, message } = messageInstance;
+
+	let member = await database.members.findOne({
+		userId: message.author.id,
+		guildId: message.guildId!,
+	});
+
+	if (!member) {
+		await database.members.createOne({
+			userId: message.author.id,
+			guildId: message.guildId!,
+		});
+		member = await database.members.findOne({
+			userId: message.author.id,
+			guildId: message.guildId!,
+		});
+	}
+
+	let currentStats = member!.stats!,
+		newStats = member!.stats!;
+
+	newStats.messageCount = currentStats.messageCount! + 1;
+	newStats.xp = currentStats.xp! + 1;
+
+	let hasLevelUp = newStats.xp === Math.floor(5 ** 1.1 * currentStats.level!);
+	if (hasLevelUp) {
+		newStats.xp = 0;
+		newStats.level = currentStats.level! + 1;
+	}
+
+	await database.members.updateOne(
+		{
+			userId: message.author.id,
+			guildId: message.guildId!,
+		},
+		{
+			stats: newStats,
 		}
 	);
 
-	if (update === "ug") await database.guilds.add({ id: message.guildId! });
-	if (update === "uu")
-		await database.users.add(message.guildId!, {
-			id: message.author.id,
-		});
-
-	onMessage(message);
+	if (hasLevelUp) {
+		methods.send(
+			`${randomItem(
+				"Congratulations",
+				"Well done",
+				"Yay"
+			)} ${message.author.toString()} ! `.concat(
+				`You just leveled up to level ${inlineCode(
+					newStats.level!.toString()
+				)} ${reactions.smile.random}`
+			)
+		);
+	}
 };
+
+const databaseMiddleware = {
+	listeners: {
+		onMessage,
+	},
+};
+
+export default databaseMiddleware;
