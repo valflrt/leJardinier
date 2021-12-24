2abad4dd6fce3f609511097fd68d448c882f64c0
valflrt <vfleurit@gmail.com>
2021-12-24 09:59:19 +0100

    fixed api/youtube.ts

--- src/managers/api/youtube.ts
+++ src/managers/api/youtube.ts
@@ -4,6 +4,31 @@ import config from "../../config";
 
 let youtube = google.youtube("v3");
 
+const validYoutubeDomains = new Set([
+  "youtube.com",
+  "www.youtube.com",
+  "m.youtube.com",
+  "music.youtube.com",
+  "gaming.youtube.com",
+]);
+const getVideoId = (url: string): string | null => {
+  const parsedUrl = new URL(url);
+  let id = parsedUrl.searchParams.get("v");
+
+  if (id) return id;
+  else if (
+    /^https?:\/\/(youtu\.be\/|(www\.)?youtube\.com\/(embed|v|shorts)\/)/.test(
+      url
+    )
+  ) {
+    const paths = parsedUrl.pathname.split("/");
+    return parsedUrl.host === "youtu.be" ? paths[1] : paths[2];
+  } else if (parsedUrl.hostname && !validYoutubeDomains.has(parsedUrl.hostname))
+    return null;
+  if (!id) return null;
+  else return id.substring(0, 11);
+};
+
 class YoutubeAPI {
   async searchVideo(q: string) {
     let res = await youtube.search.list({
@@ -20,7 +45,8 @@ class YoutubeAPI {
   }
 
   async getVideoFromUrl(url: string): Promise<youtube_v3.Schema$Video | null> {
-    // TODO: use ytdl url parser
+    let id = getVideoId(url);
+    if (!id) return null;
     let res = await youtube.videos.list({
       key: config.secrets.youtubeApiKey,
       part: ["id", "snippet"],
