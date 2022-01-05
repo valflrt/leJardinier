60ff1a83ce169a949535c4ec7524ddd1c6ee9641
valflrt <vfleurit@gmail.com>
2022-01-05 19:17:51 +0100

    fixed database "guild" schema

--- src/managers/database/schemas/guild.ts
+++ src/managers/database/schemas/guild.ts
@@ -1,9 +1,9 @@
-import { VideoDetails } from "../../../middlewares/music/types";
+import { ITrack } from "../../../middlewares/music/classes/track";
 
 import AutoroleSchema from "./autorole";
 
 export default class GuildSchema {
   public id!: string;
-  public playlist?: VideoDetails[] = [];
+  public playlist?: ITrack[] = [];
   public autorole?: AutoroleSchema | null = null;
 }
