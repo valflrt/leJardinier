90d5415a6262675986e40b913ae9a17148406cc6
valflrt <vfleurit@gmail.com>
2022-01-15 11:52:03 +0100

    edited commands/index.ts

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,16 +1,13 @@
 import CCommandList from "../managers/commands/classes/commandList";
 
-// utility commands
 import help from "./commands/help";
 import time from "./commands/time";
 import invite from "./commands/invite";
 
-// moderation commands
 import autorole from "./commands/autorole";
 import kick from "./commands/kick";
 import ban from "./commands/ban";
 
-// fun commands
 import hey from "./commands/hey";
 import boolean from "./commands/boolean";
 import percentage from "./commands/percentage";
@@ -18,32 +15,26 @@ import choose from "./commands/choose";
 import reverse from "./commands/reverse";
 import morse from "./commands/morse";
 
-// user commands
 import profilePicture from "./commands/pp";
 import stats from "./commands/stats";
 
-// music command
 import music from "./commands/music";
 
-// exporting commands object
-
-let commandList = new CCommandList();
-
-commandList
+let commandList = new CCommandList()
   .addCommand(() => help, "Utility")
   .addCommand(() => time, "Utility")
   .addCommand(() => invite, "Utility")
-  .addCommand(() => autorole, "Moderation")
-  .addCommand(() => kick, "Moderation")
-  .addCommand(() => ban, "Moderation")
   .addCommand(() => hey, "Fun")
   .addCommand(() => boolean, "Fun")
   .addCommand(() => percentage, "Fun")
   .addCommand(() => choose, "Fun")
   .addCommand(() => reverse, "Fun")
   .addCommand(() => morse, "Fun")
+  .addCommand(() => music, "Music")
   .addCommand(() => profilePicture, "User Information")
   .addCommand(() => stats, "User Information")
-  .addCommand(() => music, "Music");
+  .addCommand(() => autorole, "Moderation")
+  .addCommand(() => kick, "Moderation")
+  .addCommand(() => ban, "Moderation");
 
 export default commandList;
