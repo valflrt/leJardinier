870b2ca6b31773b3985e2b6791a58eeb4bd67536
valflrt <vfleurit@gmail.com>
2022-01-28 16:46:26 +0100

    added link buttons to the website and my github

--- src/commands/commands/help/index.ts
+++ src/commands/commands/help/index.ts
@@ -1,3 +1,5 @@
+import { MessageActionRow, MessageButton } from "discord.js";
+
 import { bold, italic } from "@discordjs/builders";
 
 import Command from "../../../features/commands/classes/command";
@@ -8,29 +10,45 @@ import reactions from "../../../assets/reactions";
 import usage_cmd from "./subcommands/usage";
 import commands_cmd from "./subcommands/commands";
 import command_cmd from "./subcommands/command";
-import website_cmd from "./subcommands/website";
 
 const help_cmd = new Command({
   name: "help",
   description: "Display help panel",
   execution: async ({ actions }) => {
-    actions.sendCustomEmbed((embed) =>
-      embed
-        .setDescription(
-          `Hello I'm ${bold("Le Jardinier")} ${reactions.smile.random}\n`
-            .concat(
-              `I am an utility discord bot developed by <@${
-                "564012236851511298" /* my discord id */
-              }>.\n`
-            )
-            .concat(italic(`"le jardinier" means "the gardener" in french.\n`))
-            .concat("\n")
-            .concat(`Here are some commands you can start with:`)
-        )
-        .addFields(help_cmd.preview.embedFields)
+    actions.sendCustomEmbed(
+      (embed) =>
+        embed
+          .setDescription(
+            `Hello I'm ${bold("Le Jardinier")} ${reactions.smile.random}\n`
+              .concat(
+                `I am an utility discord bot developed by <@${
+                  "564012236851511298" /* my discord id */
+                }>.\n`
+              )
+              .concat(
+                italic(`"le jardinier" means "the gardener" in french.\n`)
+              )
+              .concat("\n")
+              .concat(`Here are some commands you can start with:`)
+          )
+          .addFields(help_cmd.preview.embedFields),
+      {
+        components: [
+          new MessageActionRow().setComponents([
+            new MessageButton()
+              .setLabel("Website")
+              .setURL(`https://valflrt.github.io/lejardinier/`)
+              .setStyle("LINK"),
+            new MessageButton()
+              .setLabel("Developer")
+              .setURL(`https://github.com/valflrt`)
+              .setStyle("LINK"),
+          ]),
+        ],
+      }
     );
   },
-  commands: [usage_cmd, commands_cmd, command_cmd, website_cmd],
+  commands: [usage_cmd, commands_cmd, command_cmd],
 });
 
 export default help_cmd;
