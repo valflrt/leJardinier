8635781671f47a3f1df6260194cea3554ce0f73c
valflrt <vfleurit@gmail.com>
2022-01-28 16:26:56 +0100

    deactivated commands ban and kick

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,12 +1,12 @@
-import CCommandList from "../features/commands/classes/commandList";
+import CommandList from "../features/commands/classes/commandList";
 
 import help_cmd from "./commands/help";
 import time_cmd from "./commands/time";
 import invite_cmd from "./commands/invite";
 
 import autorole_cmd from "./commands/autorole";
-import kick_cmd from "./commands/kick";
-import ban_cmd from "./commands/ban";
+// import kick_cmd from "./commands/kick";
+// import ban_cmd from "./commands/ban";
 
 import hey_cmd from "./commands/hey";
 import boolean_cmd from "./commands/boolean";
@@ -20,21 +20,21 @@ import rank_cmd from "./commands/rank";
 
 import music_cmd from "./commands/music";
 
-let commandList = new CCommandList()
-  .addCommand(() => help_cmd, "Utility")
-  .addCommand(() => time_cmd, "Utility")
-  .addCommand(() => invite_cmd, "Utility")
-  .addCommand(() => hey_cmd, "Fun")
-  .addCommand(() => boolean_cmd, "Fun")
-  .addCommand(() => percentage_cmd, "Fun")
-  .addCommand(() => choose_cmd, "Fun")
-  .addCommand(() => reverse_cmd, "Fun")
-  .addCommand(() => morse_cmd, "Fun")
-  .addCommand(() => music_cmd, "Music")
-  .addCommand(() => profilePicture_cmd, "User Information")
-  .addCommand(() => rank_cmd, "User Information")
-  .addCommand(() => autorole_cmd, "Moderation")
-  .addCommand(() => kick_cmd, "Moderation")
-  .addCommand(() => ban_cmd, "Moderation");
+let commandList = new CommandList()
+  .addCommand(help_cmd, "Utility")
+  .addCommand(time_cmd, "Utility")
+  .addCommand(invite_cmd, "Utility")
+  .addCommand(hey_cmd, "Fun")
+  .addCommand(boolean_cmd, "Fun")
+  .addCommand(percentage_cmd, "Fun")
+  .addCommand(choose_cmd, "Fun")
+  .addCommand(reverse_cmd, "Fun")
+  .addCommand(morse_cmd, "Fun")
+  .addCommand(music_cmd, "Music")
+  .addCommand(profilePicture_cmd, "User Information")
+  .addCommand(rank_cmd, "User Information")
+  .addCommand(autorole_cmd, "Moderation");
+//.addCommand(kick_cmd, "Moderation")
+//.addCommand(ban_cmd, "Moderation")
 
 export default commandList;
