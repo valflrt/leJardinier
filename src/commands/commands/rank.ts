fc699be2ff7d7fbfc3631434f915983440f12d2b
valflrt <vfleurit@gmail.com>
2022-02-02 19:41:37 +0100

    enhance the rank panel generated by command "rank"

--- src/commands/commands/rank.ts
+++ src/commands/commands/rank.ts
@@ -1,9 +1,9 @@
 import { MessageAttachment, MessageEmbed } from "discord.js";
-import Canvas from "canvas";
 
 import Command from "../../features/commands/classes/command";
+import UserStats from "../../features/userStats";
+
 import database from "../../features/database";
-import Factorize from "../../features/factorize";
 
 const rank_cmd = new Command({
   name: "rank",
@@ -16,146 +16,22 @@ const rank_cmd = new Command({
 
     let memberFromDB = await database.members.findOne({
       userId: member.id,
+      guildId: message.guildId!,
     });
 
     if (!memberFromDB?.stats)
       return actions.sendTextEmbed(`Couldn't find user stats !`);
 
-    let XP = memberFromDB.stats!.xp!;
-    let levelMaxXP = Math.floor(5 ** 1.1 * memberFromDB.stats!.level!);
-
-    let width = 400;
-    let height = 150;
-
-    const canvas = Canvas.createCanvas(width, height);
-    const ctx = canvas.getContext("2d");
-
-    ctx.textBaseline = "middle";
-
-    // frame
-
-    let linePad = 10;
-
-    let frameRadius = 5;
-
-    ctx.beginPath();
-    ctx.moveTo(0, frameRadius);
-    ctx.arcTo(0, 0, linePad, 0, frameRadius); // top left corner
-    // alt method: ctx.arc(width - radiusPad, radiusPad, borderRadius, -Math.PI / 2, Math.PI);
-    ctx.lineTo(width - linePad, 0); // top line
-    ctx.arcTo(width, 0, width, linePad, frameRadius); // top right corner
-    ctx.lineTo(width, height - linePad); // right line
-    ctx.arcTo(width, height, width - linePad, height, frameRadius); // bottom right corner
-    ctx.lineTo(linePad, height); // bottom line
-    ctx.arcTo(0, height, 0, height - linePad, frameRadius); // bottom left corner
-    ctx.lineTo(0, linePad); // left line
-    ctx.fillStyle = "rgba(20, 20, 20, 0.2)";
-    ctx.fill();
-    ctx.closePath();
-
-    // display username
-
-    let tagX = 55; // x coordinate to write the user tag
-    let leftMargin = 110;
-    let tagFontSize = 30;
-
-    ctx.font = `bold ${tagFontSize}px Sans`;
-    while (leftMargin + ctx.measureText(member.tag).width > width - 20) {
-      tagFontSize -= 0.1;
-      ctx.font = `bold ${tagFontSize}px Sans`;
-    }
-
-    ctx.fillStyle = "#ffffff";
-    ctx.fillText(member.username, leftMargin, tagX);
-    ctx.fillStyle = "#878787";
-    ctx.fillText(
-      `#${member.discriminator}`,
-      ctx.measureText(member.username).width + leftMargin,
-      tagX
-    );
-
-    // display level
-
-    ctx.fillStyle = "#ffffff";
-    ctx.font = "18px Sans";
-    ctx.fillText(`Level ${memberFromDB.stats.level}`, leftMargin + 2, 82);
-
-    // main values to draw the line graph
-
-    let barHeight = 8;
-    let barHalfHeight = barHeight / 2;
-    let barLength = 200;
-    let barStart = leftMargin + barHalfHeight;
-    let barEnd = barStart + barLength;
-    let barTop = 95;
-    let barBottom = barTop + barHeight;
-    let barMiddle = barTop + barHalfHeight;
-
-    // display xp at the end of the bar
-
-    ctx.font = "bold 14px Sans";
-    ctx.fillText(
-      `${Factorize.fromNumber(XP)}/${Factorize.fromNumber(levelMaxXP)}`,
-      barEnd + 12,
-      barMiddle
-    );
-
-    // display xp bar graph bg
-
-    ctx.beginPath();
-    ctx.fillStyle = "rgba(255, 255, 255, 0.2)";
-    ctx.lineTo(barStart, barTop);
-    ctx.lineTo(barEnd, barTop);
-    ctx.arc(barEnd, barMiddle, barHalfHeight, -Math.PI / 2, Math.PI / 2); // right end
-    ctx.lineTo(barEnd, barBottom);
-    ctx.lineTo(barStart, barBottom);
-    ctx.arc(barStart, barMiddle, barHalfHeight, Math.PI / 2, -Math.PI / 2); // left end
-    ctx.fill();
-    ctx.closePath();
-
-    // display xp line graph
-
-    let xpBarEnd = (XP / levelMaxXP) * barLength + barStart;
-
-    ctx.beginPath();
-    ctx.fillStyle = "#4CE821";
-    ctx.lineTo(barStart, barTop);
-    ctx.lineTo(xpBarEnd, barTop);
-    ctx.arc(xpBarEnd, barMiddle, barHalfHeight, -Math.PI / 2, Math.PI / 2);
-    ctx.lineTo(xpBarEnd, barBottom);
-    ctx.lineTo(barStart, barBottom);
-    ctx.arc(barStart, barMiddle, barHalfHeight, Math.PI / 2, -Math.PI / 2);
-    ctx.fill();
-    ctx.closePath();
-
-    // crop around avatar image
-
-    let avatarRadius = 31;
-    ctx.beginPath();
-    ctx.arc(60, height / 2, avatarRadius, 0, Math.PI * 2);
-    ctx.closePath();
-    ctx.clip();
-
-    let avatarURL = member.avatarURL({ format: "png", size: 64 });
-    if (avatarURL) {
-      const avatar = await Canvas.loadImage(avatarURL);
-      ctx.drawImage(
-        avatar,
-        60 - avatarRadius,
-        height / 2 - avatarRadius,
-        64,
-        64
-      );
-    } else {
-      ctx.rect(56, 61, 128, 128);
-      ctx.fillStyle = "rgba(200, 200, 200, 0.5)";
-      ctx.fill();
-    }
+    let preview = await new UserStats(
+      member,
+      memberFromDB.stats,
+      message.guildId!
+    ).generatePreview();
 
     new MessageEmbed();
 
     actions.send({
-      files: [new MessageAttachment(canvas.toBuffer(), "stats.png")],
+      files: [new MessageAttachment(preview.toBuffer(), "stats.png")],
       embeds: [
         actions.returnCustomEmbed((embed) =>
           embed.setImage("attachment://stats.png")
---
+++ src/features/userStats/index.ts
@@ -0,0 +1,184 @@
+import { User } from "discord.js";
+import { createCanvas, loadImage, registerFont } from "canvas";
+
+import Factorize from "../factorize";
+
+import StatsSchema from "../database/schemas/stats";
+import database from "../database";
+
+export default class UserStats {
+  private member: User;
+  private stats: Required<StatsSchema>;
+  private guildId: string;
+
+  constructor(member: User, stats: StatsSchema, guildId: string) {
+    this.member = member;
+    this.stats = stats as Required<StatsSchema>;
+    this.guildId = guildId;
+  }
+
+  public async generatePreview() {
+    let XP = this.stats.xp!;
+    let levelMaxXP = Math.floor(5 ** 1.1 * this.stats.level!);
+
+    let canvasWidth = 700;
+    let canvasHeight = 200;
+
+    registerFont("src/assets/OpenSans.ttf", {
+      family: "OpenSansRegular",
+      weight: "500",
+    });
+
+    const canvas = createCanvas(canvasWidth, canvasHeight);
+    const ctx = canvas.getContext("2d");
+
+    ctx.textBaseline = "middle";
+
+    // frame
+
+    let cornerPad = 20;
+    let frameRadius = 10;
+
+    ctx.beginPath();
+    ctx.moveTo(0, frameRadius);
+    ctx.arcTo(0, 0, cornerPad, 0, frameRadius); // top left corner
+    // alt method: ctx.arc(width - radiusPad, radiusPad, borderRadius, -Math.PI / 2, Math.PI);
+    ctx.lineTo(canvasWidth - cornerPad, 0); // top line
+    ctx.arcTo(canvasWidth, 0, canvasWidth, cornerPad, frameRadius); // top right corner
+    ctx.lineTo(canvasWidth, canvasHeight - cornerPad); // right line
+    ctx.arcTo(
+      canvasWidth,
+      canvasHeight,
+      canvasWidth - cornerPad,
+      canvasHeight,
+      frameRadius
+    ); // bottom right corner
+    ctx.lineTo(cornerPad, canvasHeight); // bottom line
+    ctx.arcTo(0, canvasHeight, 0, canvasHeight - cornerPad, frameRadius); // bottom left corner
+    ctx.lineTo(0, cornerPad); // left line
+    ctx.fillStyle = "rgba(20, 20, 20, 0.2)";
+    ctx.fill();
+    ctx.closePath();
+
+    // information panel
+
+    let centerY = canvasHeight / 2;
+
+    let panelX = 200;
+    let tagFontSize = 50;
+
+    ctx.font = `bold ${tagFontSize}px OpenSans`;
+    while (panelX + ctx.measureText(this.member.tag).width > canvasWidth - 40) {
+      tagFontSize -= 0.1;
+      ctx.font = `bold ${tagFontSize}px OpenSans`;
+    }
+
+    let tagRelativeY = centerY - 36;
+
+    ctx.fillStyle = "#ffffff";
+    ctx.fillText(this.member.username, panelX, tagRelativeY);
+    ctx.fillStyle = "#878787";
+    ctx.fillText(
+      `#${this.member.discriminator}`,
+      panelX + ctx.measureText(this.member.username).width,
+      tagRelativeY
+    );
+
+    // display user info
+
+    let array = await database.members.collection
+      .find({ guildId: this.guildId })
+      .sort({ "stats.totalXp": -1 })
+      .toArray();
+
+    console.log(array);
+
+    let rank = array?.findIndex((m) => m.userId === this.member.id);
+
+    ctx.fillStyle = "#ffffff";
+    ctx.font = "26px OpenSans";
+    ctx.fillText(
+      `Level ${this.stats.level} • Server rank: ${
+        rank === 0 || rank !== undefined ? `#${rank + 1}` : "Unranked"
+      }`,
+      panelX + 4,
+      centerY + 8
+    );
+
+    // main values to draw the line graph
+
+    let barHeight = 16;
+    let barHalfHeight = barHeight / 2;
+    let barLength = 360;
+    let barLeft = panelX + barHalfHeight;
+    let barRight = barLeft + barLength;
+    let barTop = centerY + 28;
+    let barBottom = barTop + barHeight;
+    let barMiddle = barTop + barHalfHeight;
+
+    // display xp at the end of the bar
+
+    ctx.font = "bold 24px OpenSans";
+    ctx.fillText(
+      `${Factorize.fromNumber(XP)}/${Factorize.fromNumber(levelMaxXP)} xp`,
+      barRight + 24,
+      barMiddle
+    );
+
+    // display xp bar graph bg
+
+    ctx.beginPath();
+    ctx.fillStyle = "rgba(255, 255, 255, 0.2)";
+    ctx.lineTo(barLeft, barTop);
+    ctx.lineTo(barRight, barTop);
+    ctx.arc(barRight, barMiddle, barHalfHeight, -Math.PI / 2, Math.PI / 2); // right end
+    ctx.lineTo(barRight, barBottom);
+    ctx.lineTo(barLeft, barBottom);
+    ctx.arc(barLeft, barMiddle, barHalfHeight, Math.PI / 2, -Math.PI / 2); // left end
+    ctx.fill();
+    ctx.closePath();
+
+    // display xp line graph
+
+    let xpBarEnd = (XP / levelMaxXP) * barLength + barLeft;
+
+    ctx.beginPath();
+    ctx.fillStyle = "#4CE821";
+    ctx.lineTo(barLeft, barTop);
+    ctx.lineTo(xpBarEnd, barTop);
+    ctx.arc(xpBarEnd, barMiddle, barHalfHeight, -Math.PI / 2, Math.PI / 2);
+    ctx.lineTo(xpBarEnd, barBottom);
+    ctx.lineTo(barLeft, barBottom);
+    ctx.arc(barLeft, barMiddle, barHalfHeight, Math.PI / 2, -Math.PI / 2);
+    ctx.fill();
+    ctx.closePath();
+
+    // crop around avatar image
+
+    let avatarRadius = 63;
+    let avatarX = 40;
+
+    ctx.beginPath();
+    ctx.arc(
+      avatarX + avatarRadius,
+      canvasHeight / 2,
+      avatarRadius,
+      0,
+      Math.PI * 2
+    );
+    ctx.closePath();
+    ctx.clip();
+
+    let avatarURL = this.member.avatarURL({ format: "png", size: 128 });
+    if (avatarURL) {
+      const avatar = await loadImage(avatarURL);
+      ctx.drawImage(avatar, avatarX, canvasHeight / 2 - avatarRadius, 128, 128);
+    } else {
+      ctx.rect(avatarX, 61, 128, 128);
+      ctx.fillStyle = "rgba(200, 200, 200, 0.5)";
+      ctx.fill();
+    }
+
+    return canvas;
+  }
+}
