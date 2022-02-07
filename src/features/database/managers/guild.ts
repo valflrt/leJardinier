07d19845709ed1a638a95b380cdc2055740e645b
valflrt <vfleurit@gmail.com>
2022-02-07 17:24:17 +0100

    remove database managers

--- src/features/database/managers/guild.ts
+++
@@ -1,7 +0,0 @@
-import DefaultManager from "./default";
-
-import GuildSchema from "../schemas/guild";
-
-export default class GuildManager extends DefaultManager<GuildSchema> {
-  protected override schemaConstructor = GuildSchema;
-}
--- src/features/database/managers/member.ts
+++
@@ -1,7 +0,0 @@
-import DefaultManager from "./default";
-
-import MemberSchema from "../schemas/member";
-
-export default class MemberManager extends DefaultManager<MemberSchema> {
-  protected override schemaConstructor = MemberSchema;
-}
